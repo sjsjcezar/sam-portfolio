210c6d22220ef7122ac02e22a3ea03fb275f3712
sjcezar <cezarsjsj@gmail.com>
2025-06-02 04:33:48 +0800

    first commit

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
             <div className="flex justify-between items-center h-16">
                 <a href="#home" className="font-mono text-xl font-bold text-white"> 
                     {" "}
-                    Wet Banana<span className="text-blue-500"> Studios</span>{""}
+                    Wet Banana<span className="text-yellow-500"> Studios</span>{""}
                 </a>
 
                 <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" 
